c6c4f1adf1857417a16100c52c4ba52194fa18df
Tsilavina <159104790+TsilavinaZh@users.noreply.github.com>
2024-03-06 09:31:00 +0300

    Add files via upload

---
+++ TikTakToe/App.js
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text, Alert, Animated } from 'react-native';
+
+const GameBoard = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [gameOver, setGameOver] = useState(false);
+
+  useEffect(() => {
+    checkForWinner();
+    checkIfTie();
+  }, [board]);
+
+  const handlePress = (index) => {
+    if (board[index] || gameOver) return;
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+  };
+
+  const checkForWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        Alert.alert(`Player ${board[a]} has won!`);
+        setGameOver(true);
+        return;
+      }
+    }
+  };
+
+  const checkIfTie = () => {
+    if (board.every(cell => cell !== null) && !gameOver) {
+      Alert.alert("It's a tie!");
+      setGameOver(true);
+    }
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setCurrentPlayer('X');
+    setGameOver(false);
+  };
+
+  const renderCell = (index) => {
+    const opacity = new Animated.Value(1); // Initialiser la valeur d'opacité à 1
+
+    const onPress = () => {
+      if (!board[index]) {
+        handlePress(index);
+        Animated.timing(opacity, {
+          toValue: 0,
+          duration: 100,
+          useNativeDriver: true,
+        }).start(() => {
+          Animated.timing(opacity, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+          }).start();
+        });
+      }
+    };
+
+    return (
+      <TouchableOpacity key={index} style={styles.cell} onPress={onPress}>
+        <Animated.View style={{ opacity }}>
+          <Text style={styles.cellText}>{board[index]}</Text>
+        </Animated.View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.board}>
+        {Array(9).fill(null).map((_, index) => renderCell(index))}
+      </View>
+      <TouchableOpacity style={styles.resetButton} onPress={resetGame}>
+        <Text style={styles.resetButtonText}>Reset Game</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#282c34',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  board: {
+    width: 300,
+    height: 300,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: '#000',
+    borderWidth: 3,
+    borderColor: '#FFFFFF',
+  },
+  cell: {
+    width: '33.333%',
+    height: '33.333%',
+    backgroundColor: '#1e2126',
+    borderWidth: 1,
+    borderColor: '#4A4A4A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cellText: {
+    fontSize: 60,
+    fontWeight: 'bold',
+    color: '#ecf0f1',
+  },
+  resetButton: {
+    marginTop: 20,
+    backgroundColor: 'skyblue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  resetButtonText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default GameBoard;
